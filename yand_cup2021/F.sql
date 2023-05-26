2fbb8b6da460689894f9e032596aa38fb6d1e3e7
archibrat <30520653+archibrat@users.noreply.github.com>
2023-05-26 20:16:04 +0300

    Update F.sql

--- yand_cup2021/F.sql
+++ yand_cup2021/F.sql
@@ -1,52 +1,63 @@
- ;with Cte As 
+;
+WITH Cte AS
 (
-SELECT tg.track_id as track_id 
-, g.id as genre_id
-, t.name as track_name
-, 
-g.name as genre_name 
-FROM track_genre tg 
-join track t on tg.track_id = t.id 
-join genre g on tg.genre_id = g.id 
-where g.parent_genre_id is null 
-UNION all 
-SELECT tg.track_id as track_id 
-, g.id as genre_id
-, t.name as track_name
-, 
-g.name as genre_name 
-from track_genre tg 
-join genre as g on tg.genre_id = g.id 
-left join genre as g2 on g2.parent_genre_id = g.id 
-join track t on tg.track_id = t.id 
-UNION all 
-SELECT tg.track_id as track_id 
-, g2.id as genre_id
-, t.name as track_name
-, 
-g2.name as genre_name 
-from track_genre tg 
-join genre as g on tg.genre_id = g.id 
-join genre as g2 on g2.id = g.parent_genre_id 
-join track t on tg.track_id = t.id 
-UNION all 
-SELECT tg.track_id as track_id 
-, g3.id as genre_id
-, t.name as track_name
-, 
-g3.name as genre_name 
-from track_genre tg 
-join genre as g on tg.genre_id = g.id 
-join genre as g2 on g2.id = g.parent_genre_id 
-join genre as g3 on g2.parent_genre_id = g3.id 
-join track t on tg.track_id = t.id 
-) 
-select track_id
-,genre_id
-,track_name
-,genre_name FROM Cte 
-group by genre_id
-,genre_name
-,track_id
-,track_name 
-order by track_id, genre_id
+	SELECT  tg.track_id AS track_id
+	       ,g.id        AS genre_id
+	       ,t.name      AS track_name
+	       ,g.name      AS genre_name
+	FROM track_genre tg
+	JOIN track t
+	ON tg.track_id = t.id
+	JOIN genre g
+	ON tg.genre_id = g.id
+	WHERE g.parent_genre_id is null 
+	UNION ALL
+	SELECT  tg.track_id AS track_id
+	       ,g.id        AS genre_id
+	       ,t.name      AS track_name
+	       ,g.name      AS genre_name
+	FROM track_genre tg
+	JOIN genre AS g
+	ON tg.genre_id = g.id
+	LEFT JOIN genre AS g2
+	ON g2.parent_genre_id = g.id
+	JOIN track t
+	ON tg.track_id = t.id
+	UNION ALL
+	SELECT  tg.track_id AS track_id
+	       ,g2.id       AS genre_id
+	       ,t.name      AS track_name
+	       ,g2.name     AS genre_name
+	FROM track_genre tg
+	JOIN genre AS g
+	ON tg.genre_id = g.id
+	JOIN genre AS g2
+	ON g2.id = g.parent_genre_id
+	JOIN track t
+	ON tg.track_id = t.id
+	UNION ALL
+	SELECT  tg.track_id AS track_id
+	       ,g3.id       AS genre_id
+	       ,t.name      AS track_name
+	       ,g3.name     AS genre_name
+	FROM track_genre tg
+	JOIN genre AS g
+	ON tg.genre_id = g.id
+	JOIN genre AS g2
+	ON g2.id = g.parent_genre_id
+	JOIN genre AS g3
+	ON g2.parent_genre_id = g3.id
+	JOIN track t
+	ON tg.track_id = t.id
+)
+SELECT  track_id
+       ,genre_id
+       ,track_name
+       ,genre_name
+FROM Cte
+GROUP BY  genre_id
+         ,genre_name
+         ,track_id
+         ,track_name
+ORDER BY track_id
+         ,genre_id
